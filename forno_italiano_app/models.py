7dfd9f2eedf88f6ce33e2b6790f10e8452385be1
declanwatts97 <declan.watts97@gmail.com>
2025-11-06 20:23:05 +0000

    added model for booking a table to models.py

--- forno_italiano_app/models.py
+++ forno_italiano_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class Booking(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('date', 'time')  
+
+    def clean(self):
+        
+        if Booking.objects.exclude(pk=self.pk).filter(date=self.date, time=self.time).exists():
+            raise ValidationError("This time slot is already booked. Please choose another time.")
+
+    def __str__(self):
+        return f"{self.full_name} - {self.date} {self.time}"

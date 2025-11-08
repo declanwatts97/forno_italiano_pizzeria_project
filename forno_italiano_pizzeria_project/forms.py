25762396b0eaf9b839d4e369dbab42d58c548fb5
declanwatts97 <declan.watts97@gmail.com>
2025-11-08 10:17:53 +0000

    added form for booking a table

---
+++ forno_italiano_pizzeria_project/forms.py
@@ -0,0 +1,11 @@
+rom django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['full_name', 'email', 'phone_number', 'date', 'time', 'number_of_guests']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
